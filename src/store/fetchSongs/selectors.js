c9fc35802aced6f563085816881eb3d812175c9f
RafalJimi <56085348+RafalJimi@users.noreply.github.com>
2020-03-08 21:54:53 +0100

    Add files via upload

--- src/store/fetchSongs/selectors.js
+++ src/store/fetchSongs/selectors.js
@@ -2,6 +2,8 @@ export const songsCount = state => state.fetchSongs.songs.resultCount;
 
 export const songsArray = state => state.fetchSongs.songs.results;
 
+export const songsArrayToPlay = state => state.fetchSongs.songs;
+
 export const loadingSongs = state => state.fetchSongs.songsLoading;
 
 export const isError = state => state.fetchSongs.isError;
