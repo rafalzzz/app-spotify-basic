8aab288127f82fc02c834c9cbbafc3717b102447
RafalJimi <56085348+RafalJimi@users.noreply.github.com>
2020-03-15 21:08:14 +0100

    Delete selectors.js

--- src/store/songs/selectors.js
+++
@@ -1,7 +0,0 @@
-export const songsCount = state => state.songs.songs.resultCount;
-
-export const songsArray = state => state.songs.songs.results;
-
-export const loadingSongs = state => state.songs.songsLoading;
-
-export const isError = state => state.songs.isError;
